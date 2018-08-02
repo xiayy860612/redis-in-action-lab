cc0bda922d8f7f34f827ea186751955d73b0715a
Yangyang Xia <Yangyang.Xia@ygomi.com>
2018-08-02 18:32:37 +0800

    create lab06 02 project

---
+++ lab06-2/src/main/java/com/s2u2m/lab/serviceinaction/lab0602/Lab0602App.java
@@ -0,0 +1,17 @@
+package com.s2u2m.lab.serviceinaction.lab0602;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * Lab0602App
+ * Create by Yangyang.xia on 8/2/18
+ */
+@SpringBootApplication
+public class Lab0602App extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab0602App.class, args);
+    }
+}
